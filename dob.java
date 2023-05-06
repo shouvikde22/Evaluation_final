b79314da08043cb5743d140b42a1f66c778f948d
shouvikde22 <shouvikde7@gmail.com>
2023-05-06 15:42:11 +0530

    new changes

--- dob.java
+++ dob.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    String dob = "06/10/2000";
+    String dob = "10/10/2000";
     System.out.println("My date of birth is " + dob);
   }
 }
